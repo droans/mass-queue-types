900b7168ada45e916b6f60b4b60a0a8bdff5a88e
Michael Carroll <mjoecarroll@gmail.com>
2026-01-05 16:19:32 -0500

    Fix: Wrong interface/type

--- packages/mass_queue/actions/get_playlist_tracks.ts
+++ packages/mass_queue/actions/get_playlist_tracks.ts
@@ -1,10 +1,10 @@
 import { baseMassQueueServiceWithResponseSchema, getCollectionDataServiceData, getTracksServiceResponse } from "../utils.js";
 
-export type getPlaylistTracksServiceResponse = getTracksServiceResponse;
-
-export interface getPlaylistTracksServiceData extends getCollectionDataServiceData {
+export interface getPlaylistTracksServiceResponse extends getTracksServiceResponse {
   position: number
 }
+
+export type getPlaylistTracksServiceData = getCollectionDataServiceData;
 
 export interface getPlaylistTracksServiceSchema extends baseMassQueueServiceWithResponseSchema {
   service: 'get_playlist_tracks';
